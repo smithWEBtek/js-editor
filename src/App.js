0a95b9499112c4f83e3d0f0ddbd073826086ae3f
Smith, Bradley <Bradley.Smith@hmhco.com>
2023-03-09 22:36:11 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [jsonData, setJsonData] = React.useState("")
+  const handleInputChange = (event) => {
+    event.preventDefault()
+    setJsonData(event.target.value)
+  }
+
+  const replacements = {
+    tilepoolfrac: "dropdownselect",      
+    dropdownselectfrac: "dropdownselect",      
+    TilePoolTemplate: "TilePool2",
+    tilepool: "dropdownselect",
+  }
+
+  const copyUpdatedData = () => {
+    let inputTextArea = document.getElementById("copy-text-input");
+    navigator.clipboard.writeText(inputTextArea.value)
+    inputTextArea.innerText('Updated JSON copied to clipboard')
+  }
+
+  React.useEffect(() => {
+    setJsonData(jsonData.replace(/tilepool|TilePoolTemplate|tilepoolfrac|dropdownselectfrac/g, matched => replacements[matched]));
+  },[jsonData])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <textarea
+          id="copy-text-input" 
+          type="text" 
+          style={{height: '400px', width: '1000px'}}
+          value={jsonData}
+          placeholder={'Paste JSON here, then click Copy to Clipboard below'}
+          onChange={handleInputChange}
+        />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button onClick={copyUpdatedData}>Copy to clipboard</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
